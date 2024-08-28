849dc61871c4804c9754a0fc36fbaa8b4bd4892b
aluno <aluno@udicentro.local>
2024-08-27 22:14:19 -0300

    commit apos o rebase

--- exercicio-branches/src/Teste.java
+++ exercicio-branches/src/Teste.java
@@ -1,5 +1,6 @@
 public class Teste {
     private int idade;
+    private String nome;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Teste");
     }
